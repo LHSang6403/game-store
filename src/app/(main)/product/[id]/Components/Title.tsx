b52ab5d15ca18505e08d88fc5195466b63f4475d
LHSang <lhsang64.contact@gmail.com>
2024-02-16 22:35:48 +0700

    fixex Title

--- src/app/(main)/product/[id]/Components/Title.tsx
+++ src/app/(main)/product/[id]/Components/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <div className="absolute top-2 left-1/2 sm:w-screen transform -translate-x-1/2">
+    <div className="absolute top-2 left-1/2 sm:w-screen sm:px-2 transform -translate-x-1/2">
       <h2 className="max-w-[650px] sm:max-w-full line-clamp-1 overflow-ellipsis text-center text-2xl font-medium text-foreground/90">
         Brand Name
       </h2>
