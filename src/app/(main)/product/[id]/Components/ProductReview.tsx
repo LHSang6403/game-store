d819df014c61a0df70714f8a891dc7da47512c79
LHSang <lhsang64.contact@gmail.com>
2024-02-16 22:05:16 +0700

    fixed review form

--- src/app/(main)/product/[id]/Components/ProductReview.tsx
+++ src/app/(main)/product/[id]/Components/ProductReview.tsx
@@ -1,38 +1,14 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { toast } from "sonner";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@components/ui/form";
-import { Textarea } from "@components/ui/textarea";
-import { Input } from "@components/ui/input";
-import { Button } from "@components/ui/button";
 import Image from "next/image";
-
-const FormSchema = z.object({
-  name: z.string(),
-  information: z.string(),
-  review: z.string(),
-});
+import ReviewForm from "./ReviewForm";
 
 export default function ProductReview() {
   return (
-    <div className="w-[70%] h-fit mx-auto rounded-3xl bg-background flex flex-row gap-10 xl:gap-6 sm:gap-4 justify-center">
+    <div className="w-[70%] h-fit mx-auto rounded-3xl bg-background flex flex-row gap-6 sm:gap-4 justify-center">
       <div className="w-fit h-full overflow-auto flex flex-col gap-6">
         <PersonReview />
         <PersonReview />
       </div>
-      {/* <div className="w-1/2 xl:w-full"> */}
       <ReviewForm />
-      {/* </div> */}
     </div>
   );
 }
@@ -61,95 +37,6 @@ function PersonReview() {
           layout="fill"
         />
       </div>
-    </div>
-  );
-}
-
-function ReviewForm() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      name: "",
-      information: "",
-      review: "",
-    },
-  });
-
-  async function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(data);
-    toast.success("Review submitted successfully.");
-  }
-
-  return (
-    <div className="w-72 h-fit flex flex-col gap-2 border border-foreground/20 py-4 px-3 rounded-md">
-      <h2 className="text-xl font-semibold text-center">Share your review</h2>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full space-y-3"
-        >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Your name"
-                    {...field}
-                    type="text"
-                    onChange={field.onChange}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="information"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Extra information</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Your information"
-                    {...field}
-                    type="text"
-                    onChange={field.onChange}
-                  />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="review"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Review</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Your review"
-                    {...field}
-                    onChange={field.onChange}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button
-            type="submit"
-            className="w-full bg-foreground text-background"
-          >
-            Submit
-          </Button>
-        </form>
-      </Form>
     </div>
   );
 }
