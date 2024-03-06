32081ac080393064982601291b49977d0122595c
LHSang <lhsang64.contact@gmail.com>
2024-03-06 23:01:35 +0700

    done range time picker

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -40,7 +40,7 @@ function Calendar({
         ),
         day_range_end: "day-range-end",
         day_selected:
-          "bg-foreground/20 text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-foreground/20 focus:text-primary-foreground",
+          "bg-foreground/20 text-primary-foreground hover:bg-foreground/20 hover:text-primary-foreground focus:bg-foreground/20 focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
