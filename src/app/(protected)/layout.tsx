99be1503752834fd414f524cd23c76fab637d0a2
LHSang <lhsang64.contact@gmail.com>
2024-03-07 14:49:16 +0700

    fixed not access

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,7 +2,6 @@
 
 import Header from "@/components/Layout/Header/Header";
 import Footer from "@/components/Layout/Footer/Footer";
-import { readUserSession } from "@app/_actions/user";
 import Link from "next/link";
 import { useSession } from "@/zustand/useSession";
 
@@ -11,19 +10,20 @@ export default async function ProtectedLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const session = await readUserSession();
   const { session } = useSession();
   console.log(session);
 
   if (!session || !("role" in session)) {
     return (
       <div className="mt-10 flex flex-col items-center">
-        <span className="text-xl font-medium">You are not logged in. </span>
+        <span className="text-xl font-medium">
+          You can not access this area.{" "}
+        </span>
         <Link
           className="text-base text-foreground/80 hover:text-foreground"
           href="/auth"
         >
-          Login
+          Login another account.
         </Link>
       </div>
     );
