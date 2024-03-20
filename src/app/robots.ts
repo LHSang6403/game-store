69a63434b0b4e9e5bb0e717f1afd891324a26219
LHSang <lhsang64.contact@gmail.com>
2024-03-20 15:03:31 +0700

    fixing baseUrl

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,16 +1,16 @@
-// const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-//   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-//   : "http://localhost:3000";
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
 
-// export default function robots() {
-//   return {
-//     rules: [
-//       {
-//         userAgent: "*",
-//         disallow: ["/dashboard"],
-//       },
-//     ],
-//     sitemap: `${baseUrl}/sitemap.xml`,
-//     host: baseUrl,
-//   };
-// }
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        disallow: ["/dashboard"],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
