a399b3c7c62b925f588a8d4374a816be1ffabba6
LHSang <lhsang64.contact@gmail.com>
2024-03-22 13:36:26 +0700

    add dumped sql

--- postgres-dump/log.sql
+++ postgres-dump/log.sql
@@ -12,6 +12,26 @@
  Target Server Version : 150001 (150001)
  File Encoding         : 65001
 
- Date: 21/03/2024 22:40:26
+ Date: 22/03/2024 13:35:37
 */
 
+
+-- ----------------------------
+-- Table structure for log
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."log";
+CREATE TABLE "public"."log" (
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "created_at" timestamptz(6) NOT NULL DEFAULT now(),
+  "title" text COLLATE "pg_catalog"."default",
+  "actor" text COLLATE "pg_catalog"."default",
+  "type" text COLLATE "pg_catalog"."default",
+  "result" text COLLATE "pg_catalog"."default"
+)
+;
+ALTER TABLE "public"."log" OWNER TO "postgres";
+
+-- ----------------------------
+-- Primary Key structure for table log
+-- ----------------------------
+ALTER TABLE "public"."log" ADD CONSTRAINT "log_pkey" PRIMARY KEY ("id");
