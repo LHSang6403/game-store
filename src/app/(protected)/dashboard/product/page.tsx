fd5e5d0bab7ff73e53b857609b30fcccae11d9f4
LHSang <lhsang64.contact@gmail.com>
2024-05-14 22:08:30 +0700

    fixed slow dashboard

--- src/app/(protected)/dashboard/product/page.tsx
+++ src/app/(protected)/dashboard/product/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   columns,
   columns_headers,
@@ -6,9 +8,14 @@ import { DataTable } from "@components/Table/DataTable";
 import Link from "next/link";
 import { readProducts } from "@/app/_actions/product";
 import { ProductType } from "@utils/types/index";
+import { useQuery } from "@tanstack/react-query";
 
-export default async function Page() {
-  const products = await readProducts({ limit: 100, offset: 0 });
+export default function Page() {
+  const { data: products } = useQuery({
+    queryKey: ["products", "all"],
+    queryFn: () => readProducts({ limit: 100, offset: 0 }),
+    staleTime: 3600 * 60,
+  });
 
   return (
     <section className="w-full overflow-auto">
@@ -22,15 +29,13 @@ export default async function Page() {
         </Link>
       </div>
       <div className="w-full overflow-hidden">
-        {products.data && (
-          <DataTable
-            columns={columns}
-            data={products.data as ProductType[]}
-            isPaginationEnabled={true}
-            columns_headers={columns_headers}
-            searchPlaceholder="Tên sản phẩm..."
-          />
-        )}
+        <DataTable
+          columns={columns}
+          data={products?.data ?? ([] as ProductType[])}
+          isPaginationEnabled={true}
+          columns_headers={columns_headers}
+          searchPlaceholder="Tên sản phẩm..."
+        />
       </div>
     </section>
   );
