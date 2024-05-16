982606bbc703dc984518f91a167610e0d3af73de
LHSang <lhsang64.contact@gmail.com>
2024-05-16 16:31:26 +0700

    fixing blog

--- src/app/(main)/blog/[id]/page.tsx
+++ src/app/(main)/blog/[id]/page.tsx
@@ -4,9 +4,14 @@ import { readBlogById } from "@app/_actions/blog";
 import { useQuery } from "@tanstack/react-query";
 import BlogContent from "@app/(main)/blog/[id]/Components/BlogContent";
 import BlogThumbnails from "@app/(main)/blog/[id]/Components/BlogThumbnails";
+import Loading from "@app/loading";
 
 export default function page({ params }: { params: { id: string } }) {
-  const { data: blog, isSuccess } = useQuery({
+  const {
+    data: blog,
+    isLoading,
+    isSuccess,
+  } = useQuery({
     queryKey: ["blog", params.id],
     queryFn: () => readBlogById(params.id),
     staleTime: 60 * (60 * 1000),
@@ -14,19 +19,25 @@ export default function page({ params }: { params: { id: string } }) {
 
   return (
     <div className="flex min-h-screen w-full flex-col items-center gap-4 rounded-3xl bg-gradient-to-t from-accent pt-4">
-      {isSuccess && (
+      {isLoading ? (
+        <Loading />
+      ) : (
         <>
-          <h1 className="max-w-[900px] px-8 text-center text-4xl font-bold xl:px-6 sm:px-2">
-            {blog?.data?.title}
-          </h1>
-          <p className="max-w-[1000px] p-8 xl:p-6 sm:p-2">
-            {blog?.data?.description}
-          </p>
-          {blog?.data?.thumbnails && (
-            <BlogThumbnails thumbs={blog?.data?.thumbnails} />
-          )}
-          {blog && blog.data && blog.data.content && (
-            <BlogContent blogContent={blog.data.content} />
+          {isSuccess && (
+            <>
+              <h1 className="max-w-[900px] px-8 text-center text-4xl font-bold xl:px-6 sm:px-2">
+                {blog?.data?.title}
+              </h1>
+              <p className="max-w-[1000px] p-8 xl:p-6 sm:p-2">
+                {blog?.data?.description}
+              </p>
+              {blog?.data?.thumbnails && (
+                <BlogThumbnails thumbs={blog?.data?.thumbnails} />
+              )}
+              {blog?.data?.content && (
+                <BlogContent blogContent={blog.data.content} />
+              )}
+            </>
           )}
         </>
       )}
--- src/app/(main)/product/[id]/page.tsx
+++ src/app/(main)/product/[id]/page.tsx
@@ -4,11 +4,13 @@ import ProductDetail from "@app/(main)/product/[id]/Components/ProductDetail";
 import ProductDescription from "@app/(main)/product/[id]/Components/ProductDescription";
 import useProductQuery from "@/hooks/useProductQuery";
 import Template from "@app/(protected)/template";
+import Loading from "@app/loading";
 
-export default function Product({ params }: { params: { id: string } }) {
+export default function page({ params }: { params: { id: string } }) {
   const {
     data: product,
     error,
+    isLoading,
     isSuccess,
   } = useProductQuery({ id: params.id });
 
@@ -18,16 +20,26 @@ export default function Product({ params }: { params: { id: string } }) {
 
   return (
     <>
-      {isSuccess && product && (
-        <Template>
-          <div className="flex h-fit min-h-screen w-full flex-col gap-10 pb-10">
-            <ProductDetail product={product} />
-            <h2 className="text-center text-2xl font-medium">Mô tả chi tiết</h2>
-            {product.product_description.content && (
-              <ProductDescription description={product.product_description} />
-            )}
-          </div>
-        </Template>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {isSuccess && product && (
+            <Template>
+              <div className="flex h-fit min-h-screen w-full flex-col gap-10 pb-10">
+                <ProductDetail product={product} />
+                <h2 className="text-center text-2xl font-medium">
+                  Mô tả chi tiết
+                </h2>
+                {product.product_description.content && (
+                  <ProductDescription
+                    description={product.product_description}
+                  />
+                )}
+              </div>
+            </Template>
+          )}
+        </>
       )}
     </>
   );
