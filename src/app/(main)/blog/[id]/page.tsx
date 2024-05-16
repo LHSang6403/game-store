85ae49703600b9e05e15d9388ba7a5ac33b8710d
LHSang <lhsang64.contact@gmail.com>
2024-05-16 16:24:30 +0700

    fixing blog

--- src/app/(main)/blog/[id]/page.tsx
+++ src/app/(main)/blog/[id]/page.tsx
@@ -1,23 +1,34 @@
+"use client";
+
 import { readBlogById } from "@app/_actions/blog";
+import { useQuery } from "@tanstack/react-query";
 import BlogContent from "@app/(main)/blog/[id]/Components/BlogContent";
 import BlogThumbnails from "@app/(main)/blog/[id]/Components/BlogThumbnails";
 
-export default async function page({ params }: { params: { id: string } }) {
-  const blog = await readBlogById(params.id);
+export default function page({ params }: { params: { id: string } }) {
+  const { data: blog, isSuccess } = useQuery({
+    queryKey: ["blog", params.id],
+    queryFn: () => readBlogById(params.id),
+    staleTime: 60 * (60 * 1000),
+  });
 
   return (
     <div className="flex min-h-screen w-full flex-col items-center gap-4 rounded-3xl bg-gradient-to-t from-accent pt-4">
-      <h1 className="max-w-[900px] px-8 text-center text-4xl font-bold xl:px-6 sm:px-2">
-        {blog?.data?.title}
-      </h1>
-      <p className="max-w-[1000px] p-8 xl:p-6 sm:p-2">
-        {blog?.data?.description}
-      </p>
-      {blog?.data?.thumbnails && (
-        <BlogThumbnails thumbs={blog?.data?.thumbnails} />
-      )}
-      {blog && blog.data && blog.data.content && (
-        <BlogContent blogContent={blog.data.content} />
+      {isSuccess && (
+        <>
+          <h1 className="max-w-[900px] px-8 text-center text-4xl font-bold xl:px-6 sm:px-2">
+            {blog?.data?.title}
+          </h1>
+          <p className="max-w-[1000px] p-8 xl:p-6 sm:p-2">
+            {blog?.data?.description}
+          </p>
+          {blog?.data?.thumbnails && (
+            <BlogThumbnails thumbs={blog?.data?.thumbnails} />
+          )}
+          {blog && blog.data && blog.data.content && (
+            <BlogContent blogContent={blog.data.content} />
+          )}
+        </>
       )}
     </div>
   );
