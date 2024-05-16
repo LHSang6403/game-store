2cb7a8f6a3eecca473a6ec752b73fbe48a124415
LHSang <lhsang64.contact@gmail.com>
2024-05-16 23:21:02 +0700

    fixing blogs

--- src/app/(main)/blog/[id]/page.tsx
+++ src/app/(main)/blog/[id]/page.tsx
@@ -9,6 +9,7 @@ import Loading from "@app/(main)/blog/[id]/Components/BlogLoadingSkeleton";
 export default function page({ params }: { params: { id: string } }) {
   const {
     data: blog,
+    error,
     isLoading,
     isSuccess,
   } = useQuery({
@@ -17,24 +18,28 @@ export default function page({ params }: { params: { id: string } }) {
     staleTime: 60 * (60 * 1000),
   });
 
+  if (error) {
+    throw new Error(error.message || "Không tìm thấy bài viết.");
+  }
+
   return (
     <div className="flex min-h-screen w-full flex-col items-center gap-4 rounded-3xl bg-gradient-to-t from-accent">
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          {isSuccess && (
+          {isSuccess && blog.data && (
             <>
               <h1 className="max-w-[900px] px-8 text-center text-4xl font-bold xl:px-6 sm:px-2">
-                {blog?.data?.title}
+                {blog.data.title}
               </h1>
               <p className="max-w-[1000px] p-8 xl:p-6 sm:p-2">
-                {blog?.data?.description}
+                {blog.data.description}
               </p>
-              {blog?.data?.thumbnails && (
+              {blog.data.thumbnails && (
                 <BlogThumbnails thumbs={blog?.data?.thumbnails} />
               )}
-              {blog?.data?.content && (
+              {blog.data.content && (
                 <BlogContent blogContent={blog.data.content} />
               )}
             </>
