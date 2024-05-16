57db9e52f5990d7f477fc2236472a75f723b6bec
LHSang <lhsang64.contact@gmail.com>
2024-05-17 00:05:42 +0700

    fixing loading

--- src/app/(main)/product/[id]/Components/ProductLoadingSkeleton.tsx
+++ src/app/(main)/product/[id]/Components/ProductLoadingSkeleton.tsx
@@ -2,14 +2,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="mt-4 grid h-fit w-full grid-cols-4 content-center gap-3 rounded-xl p-10 lg:p-6 sm:mt-24 sm:h-auto sm:gap-2">
-      <Skeleton className="col-span-4 h-[100px] rounded-xl bg-foreground/10 sm:h-[300px]" />
-      <Skeleton className="col-span-4 h-[100px] rounded-xl bg-foreground/10 sm:h-[300px]" />
-      <Skeleton className="col-span-1 h-[100px] rounded-xl bg-foreground/10 sm:h-[60px]" />
-      <Skeleton className="col-span-3 h-[100px] rounded-xl bg-foreground/10 sm:h-[60px]" />
-      <Skeleton className="col-span-1 h-[100px] rounded-xl bg-foreground/10 sm:h-[60px]" />
-      <Skeleton className="col-span-3 h-[100px] rounded-xl bg-foreground/10 sm:h-[60px]" />
-      <Skeleton className="col-span-4 h-[250px] rounded-xl bg-foreground/10 sm:h-[300px]" />
+    <div className="mt-4 grid h-fit w-full grid-cols-4 content-center gap-3 rounded-xl p-10 lg:p-6 sm:mt-12 sm:h-auto sm:gap-2 sm:p-4">
+      <Skeleton className="col-span-4 h-20 rounded-xl bg-foreground/10" />
+      <Skeleton className="col-span-4 h-20 rounded-xl bg-foreground/10" />
+      <Skeleton className="col-span-1 h-20 rounded-xl bg-foreground/10" />
+      <Skeleton className="col-span-3 h-20 rounded-xl bg-foreground/10" />
+      <Skeleton className="col-span-1 h-20 rounded-xl bg-foreground/10" />
+      <Skeleton className="col-span-3 h-20 rounded-xl bg-foreground/10" />
+      <Skeleton className="col-span-4 h-52 rounded-xl bg-foreground/10" />
     </div>
   );
 }
