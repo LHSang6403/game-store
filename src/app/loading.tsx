fc007e815be6e57c353b1b6a3997df7b2c309b34
LHSang <lhsang64.contact@gmail.com>
2024-05-16 16:27:09 +0700

    fixing blog

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="grid h-screen w-screen grid-cols-4 content-center gap-3 rounded-xl p-10 lg:p-6 sm:mt-24 sm:h-auto">
+    <div className="grid h-screen w-screen grid-cols-4 content-center gap-3 rounded-xl p-10 lg:p-6 sm:mt-24 sm:h-auto sm:gap-2">
       <Skeleton className="col-span-4 h-[250px] rounded-xl bg-foreground/10 sm:h-[300px]" />
       <Skeleton className="col-span-1 h-[100px] rounded-xl bg-foreground/10 sm:h-[60px]" />
       <Skeleton className="col-span-3 h-[100px] rounded-xl bg-foreground/10 sm:h-[60px]" />
