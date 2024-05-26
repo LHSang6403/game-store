95a48b6f33924194661e12b5a5a1d4a536155778
LHSang <lhsang64.contact@gmail.com>
2024-05-26 16:01:28 +0700

    fixed w

--- src/app/(main)/cart/page.tsx
+++ src/app/(main)/cart/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="relative flex min-h-screen w-full flex-col gap-4 px-10 pb-10 xl:px-6 sm:px-4">
       <div className="to-hsl(222.2,84%,4%) absolute -z-10 -ml-10 h-full w-1/2 -skew-x-[20deg] bg-gradient-to-r from-foreground/10 sm:-ml-20"></div>
       <div className="to-hsl(222.2,84%,4%) absolute -z-10 ml-16 h-full w-1/2 -skew-x-[20deg] bg-gradient-to-r from-accent sm:ml-10"></div>
-      <h1 className="mt-4 text-center text-3xl font-semibold">
+      <h1 className="mt-4 bg-gradient-to-r from-[#02A9FF] to-[#8538F8] bg-clip-text text-center text-3xl font-semibold text-transparent">
         Sản phẩm đã chọn
       </h1>
       <OrderCurrent />
