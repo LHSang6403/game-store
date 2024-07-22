91551e28dd6097d44d4a1377b05ae86658848062
LHSang <lhsang64.contact@gmail.com>
2024-07-22 21:20:08 +0700

    fix input mobile

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-[#E5E7EB] bg-background px-3 py-2 text-sm file:border-0 file:text-sm file:font-medium focus:ring-0 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-[#E5E7EB] bg-background px-3 py-2 text-sm file:border-0 file:text-sm file:font-medium focus:ring-0 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 sm:text-[1rem]",
           className
         )}
         ref={ref}
