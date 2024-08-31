ec5057bbfafdc4caf60b4bdf2dd25c7aef138682
LHSang <lhsang64.contact@gmail.com>
2024-08-31 16:52:01 +0700

    feat: big category

--- src/app/(main)/product/Components/CategoryCard.tsx
+++ src/app/(main)/product/Components/CategoryCard.tsx
@@ -28,11 +28,11 @@ export default function CategoryCard({
       className={`flex h-fit w-fit flex-col items-center gap-4 overflow-ellipsis whitespace-nowrap rounded-xl from-[#9633ed51] via-[#f22b9c4c] to-[#fd7c3654] px-5 pb-3 pt-2 transition duration-300 ease-in-out hover:scale-[1.01] hover:cursor-pointer hover:bg-gradient-to-r sm:w-full
          ${categories.includes(category) ? "bg-gradient-to-r" : ""}`}
     >
-      <div className="h-20">
+      <div className="h-20 sm:h-auto">
         <Image
           alt="Category"
           src={category_img}
-          className="object-fit !relative h-[100%] max-w-[100%] sm:w-fit"
+          className="object-fit !relative h-[100%] max-w-[100%]"
           priority
           quality={100}
           fill
