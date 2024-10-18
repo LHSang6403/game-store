83cbafd2b3f1670f3fff19721e4a5ba25e40b8ca
LHSang <lhsang64.contact@gmail.com>
2024-10-18 22:01:11 +0700

    update

--- src/components/Layout/Header/DashboardHeader.tsx
+++ src/components/Layout/Header/DashboardHeader.tsx
@@ -58,7 +58,7 @@ export default function DashboardHeader() {
       </div>
       <div className="flex w-full flex-row items-center justify-end gap-4 lg:gap-4 md:gap-2">
         {pathShowResetRangeTime.includes(pathname) && (
-          <div className="rounded-[9px] bg-gradient-to-r from-cpurple via-cpink to-corange p-[1.5px] sm:hidden">
+          <div className="rounded-lg bg-gradient-to-r from-cpurple via-cpink to-corange p-[1.5px] md:rounded-md sm:hidden">
             <Button
               variant="outline"
               className="h-9 border-none px-2 outline-none ring-0"
