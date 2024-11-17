75f3b7c622001c376e4d6e639b121a852f14b0e6
LHSang <lhsang64.contact@gmail.com>
2024-11-17 10:44:19 +0700

    blog animation

--- src/components/Items/Blog.tsx
+++ src/components/Items/Blog.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { BlogType } from "@utils/types/index";
 import FadeInWhenVisible from "@components/FadeInWhenVisible";
+import ImageWrapper from "@components/ImageWrapper";
 
 export default function Blog({ data }: { data: BlogType }) {
   return (
@@ -13,7 +14,7 @@ export default function Blog({ data }: { data: BlogType }) {
         href={`/blog/${data.id}`}
       >
         <div className="flex h-full w-full flex-col overflow-hidden text-foreground/90 ">
-          <div className="h-36 max-h-44 w-full overflow-hidden rounded-lg md:h-auto md:max-h-52">
+          {/* <div className="h-36 max-h-44 w-full overflow-hidden rounded-lg md:h-auto md:max-h-52">
             <Image
               alt="A blog"
               src={
@@ -26,7 +27,20 @@ export default function Blog({ data }: { data: BlogType }) {
               quality={100}
               fill
             />
-          </div>
+          </div> */}
+          <ImageWrapper
+            src={
+              process.env.NEXT_PUBLIC_SUPABASE_URL +
+              "/storage/v1/object/public/public_files/" +
+              data.thumbnails[0]
+            }
+            alt="blog"
+            width={400}
+            height={350}
+            isLoading={!data.thumbnails[0]}
+            className="h-36 w-full md:h-52"
+            customLoadingClassName="rounded-lg rounded-tl-[24px]"
+          />
           <div className="h-fit w-full p-1.5">
             <h2 className="line-clamp-1 h-fit overflow-ellipsis font-medium leading-6">
               {data.title}
