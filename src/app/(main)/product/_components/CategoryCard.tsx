a78867a28a79890b4067c21300e87e9c33d00b33
LHSang <lhsang64.contact@gmail.com>
2024-11-20 23:54:11 +0700

    wrapper cards

--- src/app/(main)/product/_components/CategoryCard.tsx
+++ src/app/(main)/product/_components/CategoryCard.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useCallback } from "react";
-import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
 import useProductFilter from "@/zustand/useProductFilter";
 import { CategoryType } from "@/utils/types";
+import ImageWrapper from "@/components/ImageWrapper";
 
 export default function CategoryCard({ category }: { category: CategoryType }) {
   const { categories, setCategories } = useProductFilter();
@@ -21,19 +21,18 @@ export default function CategoryCard({ category }: { category: CategoryType }) {
     <Badge
       onClick={handleCategoryClick}
       variant="secondary"
-      className={`flex h-fit w-full flex-col items-center gap-4 overflow-ellipsis whitespace-nowrap rounded-xl from-[#9633ed51] via-[#f22b9c4c] to-[#fd7c3654] px-5 pb-3 pt-2 transition duration-300 ease-in-out hover:scale-[1.01] hover:cursor-pointer hover:bg-gradient-to-r md:w-56
+      className={`flex h-fit w-full flex-col items-center gap-4 overflow-ellipsis whitespace-nowrap rounded-xl from-[#9633ed51] via-[#f22b9c4c] to-[#fd7c3654] pb-4 transition duration-300 ease-in-out hover:scale-[1.01] hover:cursor-pointer hover:bg-gradient-to-r md:w-52
          ${categories.includes(category.id) ? "bg-gradient-to-r" : ""}`}
     >
-      <div className="h-20 sm:h-auto">
-        <Image
-          alt="Category"
-          src={category.image}
-          className="object-fit !relative h-[100%] max-w-[100%]"
-          priority
-          quality={100}
-          fill
-        />
-      </div>
+      <ImageWrapper
+        src={category.image}
+        width={150}
+        height={150}
+        alt="Category"
+        isLoading={!category.image}
+        className="h-36 w-full shadow-none md:h-48"
+        customLoadingClassName="rounded-lg rounded-tl-[24px]"
+      />
       <span className="">{category.name}</span>
     </Badge>
   );
